7f9ff0da5dc90915646074b562c634f2c7d1e4fc
Tim Douglas <me@timdouglas.net>
2020-06-11 17:09:33 +0100

    Added a move reticle, an enemy and the game knows the difference between them with a tag on each object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as PIXI from 'pixi.js';
+import gsap from 'gsap'
 import playerImg from './assets/player.png';
 import floorImg from './assets/floor.png';
+import moveSelectImg from './assets/moveSelect.png';
+import enemyImg from './assets/enemy.png';
 
 const app = new PIXI.Application({ width: 800, height: 800, });
 document.body.appendChild(app.view);
@@ -14,7 +17,7 @@ let mousePosX;
 let mousePosY;
 let playerPosX;
 let playerPosY;
-
+let isEnemy;
 let movePlayer = false;
 
 //Create floor object
@@ -23,26 +26,59 @@ floor.anchor.set(0.5);
 floor.x = middleScreenX;
 floor.y = middleScreenY;
 floor.interactive = true;
-floor.buttonMode = true;
+floor.tag = 'floor';
 
-floor.on('pointerdown', onClick);
+floor.on('pointerdown', clickToMove);
 
 app.stage.addChild(floor);
 
 console.log('floor added');
 
+// Create moveSelect object
+const moveSelect = new PIXI.Sprite.from(moveSelectImg);
+moveSelect.anchor.set(0.5);
+
+app.stage.addChild(moveSelect);
 
 // Create player object
 const player = new PIXI.Sprite.from(playerImg);
 player.anchor.set(0.5);
 player.x = middleScreenX
 player.y = middleScreenY
+moveSelect.alpha = 0;
 
 app.stage.addChild(player);
 
-function onClick(e) {
+// Create enemy object
+const enemy = new PIXI.Sprite.from(enemyImg);
+enemy.anchor.set(0.5);
+enemy.x = middleScreenX + 200;
+enemy.y = middleScreenY - 200;
+enemy.buttonMode = true;
+enemy.interactive = true;
+enemy.tag = 'enemy';
+
+enemy.on('pointerdown', clickEnemy);
+
+app.stage.addChild(enemy);
+
+function clickToMove(e) {
     mousePosX = Math.round(e.data.originalEvent.clientX / 10) * 10;
     mousePosY = Math.round(e.data.originalEvent.clientY / 10) * 10;
+
+    if(e.target.tag === 'floor'){
+        gsap.fromTo(moveSelect, {angle: 0}, {angle: 360, duration: 0.5, ease:"elastic"});
+        moveSelect.x = mousePosX;
+        moveSelect.y = mousePosY;
+        moveSelect.alpha = 1;
+        isEnemy = false;
+    } else if(e.target.tag === 'enemy') {
+        gsap.fromTo(moveSelect, {angle: 0}, {angle: 360, duration: 0.5, ease:"elastic"});
+        moveSelect.x = enemy.x;
+        moveSelect.y = enemy.y;
+        moveSelect.alpha = 1;
+        isEnemy = true;
+    }
 
     playerPosX = Math.round(player.x);
     playerPosY = Math.round(player.y);
@@ -60,14 +96,20 @@ function onClick(e) {
     movePlayer = true;
 }
 
-app.ticker.add (() => {        
-    if(movePlayer) {
+function clickEnemy(e) {
+    clickToMove(e);
+}
+
+app.ticker.add (() => {    
+    console.log(movePlayer);  
+    if(movePlayer && !isEnemy) {
+        console.log(`Player moving to ${mousePosX}X ${mousePosY}Y`);
         player.x += player.direction.x * 5;
         player.y += player.direction.y * 5;
     }
 
     if(Math.round(player.x / 10) * 10 === mousePosX && Math.round(player.y / 10) * 10 === mousePosY) {
-        console.log('I HERE!')
         movePlayer = false;
+        moveSelect.alpha = 0;
     }
 });
